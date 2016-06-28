58b67b61ca1d8f7929d6040f48f7c02ccc8c9cff
Piet Geursen <pietgeursen@gmail.com>
2016-06-28 21:52:13 +1200

    ready

--- components/heart.js
+++ components/heart.js
@@ -1,9 +1,12 @@
 import {html} from 'inu'
 
 function render(model, dispatch){
-  return html`<div class="heart">
-    <img src="./heart.svg">  
-  </div>`
+  return html`
+    <div class="heart" style="width: ${100 / (model + 1)}px">
+      <img src="./heart.svg">
+        ${model > 0 ? render(model - 1, dispatch) : null}
+      </img>  
+    </div>`
 }
 
 export default render
--- index.js
+++ index.js
@@ -1,24 +1,41 @@
 import {pull, start, html} from 'inu'
+import push from 'pull-pushable'
 import heart from './components/heart'
+import letter from './letter'
 const main = document.querySelector('main')
 
 const app = {
   init: function() {
-   return {model: 1, effect: 'INIT'} 
+   return {model: 100, effect: 'INIT'} 
   },
   update: function(model, action) {
+   if(model == 0) return {model: model}
+   switch(action){
+      case 'TICK':
+        return {model: model - 1}
+   }
    return {model: model} 
   },
   view: function(model, dispatch) {
     return heart(model, dispatch) 
   },
   run: function(effect, sources) {
-    
+    switch(effect){
+      case 'INIT':
+        const p = push()
+        setInterval(() => {
+          p.push('TICK')
+           console.log(letter.letter.pop())
+        }
+        , 3000)
+        return p
+    } 
   }
 }
 
 const sources = start(app)
 
 pull(sources.views(), pull.drain(function(view) {
- html.update(main, view) 
+ //html.update(main, view) 
+ main.appendChild(view)
 }))
